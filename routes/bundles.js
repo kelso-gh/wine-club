ef5ad7463f362ebf18fea0ffab0edcac76eaca45
kelso-gh <soyouwontsingg@gmail.com>
2022-11-04 10:21:43 -0400

    tweaked some css

--- routes/bundles.js
+++ routes/bundles.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const bundlesCtrl = require('../controllers/bundles')
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
-router.get('/', bundlesCtrl.index);
+router.get('/', ensureLoggedIn, bundlesCtrl.index);
 router.get('/new', ensureLoggedIn, bundlesCtrl.new);
 router.get('/:id', bundlesCtrl.show);
 router.post('/', ensureLoggedIn, bundlesCtrl.create);
